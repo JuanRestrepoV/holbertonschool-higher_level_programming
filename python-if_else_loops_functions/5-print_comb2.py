c2150fde5c3d6f8338cf3f179f472a449fb10279
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-02-08 08:30:27 -0800

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-	print("{:02d}".format(i), end="")
-	if i != 99:
-		print(", ", end="")
-	else:
-		print()
+for i in range(0, 99):
+	print("{:02d}, ".format(i), end="")
+i = i + 1
+print("{}".format(i))
