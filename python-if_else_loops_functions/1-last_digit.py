092284f6049a807ac2eb23a5334555643d943dcb
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-02-08 12:55:16 -0800

    fixed pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 0:
-	last = number % 10
+    last = number % 10
 else:
-	last = number % -10
-print("Last digit of", number, "is", last, end=' ')
+    last = number % -10
+    print("Last digit of", number, "is", last, end=' ')
 if last == 0:
-	print(f"and is 0")
+    print(f"and is 0")
 elif last > 5:
-	print(f"and is greater than 5")
+    print(f"and is greater than 5")
 else:
-	print(f"and is less than 6 and not 0")
+    print(f"and is less than 6 and not 0")
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 output = ""
 for i in range(10):
-	for j in range(i + 1, 10):
-		output += "{}{}, ".format(i, j)
+    for j in range(i + 1, 10):
+        output += "{}{}, ".format(i, j)
 print(output[:-2])
