cf4896b4246caa911a7f3501a55bcc0b45f0d707
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-02-09 12:01:37 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     lentgh = len(sys.argv)
-    j = 0	
+    j = 0
     for i in range(1, lentgh):
         j += (int(sys.argv[i]))
     print("{}".format(j))
