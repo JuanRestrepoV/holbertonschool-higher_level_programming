0f85afe3aa93a0416a60a8aefa1db4ede99aea26
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-02-09 17:16:20 -0800

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
-    else:
-        return my_list[idx]
+    for i in range(len(my_list)):
+        if i == idx:
+            return my_list[i]
