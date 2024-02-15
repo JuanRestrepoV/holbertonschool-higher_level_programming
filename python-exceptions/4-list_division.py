fdf65c524e898721d51df700874da6ff4c0b70ff
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-15 18:14:49 -0500

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range (list_length):
         try:
-            result = my_list_1[i] / my_list_2[2]
+            result = my_list_1[i] / my_list_2[i]
             new_list.append(result)
         except ZeroDivisionError:
             result = 0
