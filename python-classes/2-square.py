3c56c29a77b615671aaa043e2a73238b55200c6d
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-19 14:56:48 -0500

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,9 @@
 class Square:
     """Square with size"""
     def __init__(self, size=0):
-        """constructor of a square object"""
+        """constructor of square object."""
         if not isinstance(size, int):
             raise TypeError("size must be an intenger")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
