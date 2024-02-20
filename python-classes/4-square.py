cc428c02691f58e1f497852c4882312aa31596a6
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-20 17:47:16 -0500

    task 4 test

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,6 +6,11 @@ class Square:
     """Square with size"""
     def __init__(self, size=0):
         """constructor of square object."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an intenger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     @property
     def size(self):
         """Getter for size"""
