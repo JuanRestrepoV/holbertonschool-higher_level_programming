ea62424a025dd0489a12248089f4bc113a1d7d60
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-21 13:01:27 -0500

    task 1 test

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Initialization and methods"""
+
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
@@ -18,8 +19,10 @@ class Rectangle:
         """setter for width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+
         if value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
 
     @property
@@ -32,6 +35,8 @@ class Rectangle:
         """setter for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+
         if value < 0:
             raise ValueError("height must be >= 0")
+            
         self.__height = value
