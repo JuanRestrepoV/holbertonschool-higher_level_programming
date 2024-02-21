39e2ba1945e8b3106151e851b0487f112effd3cb
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-21 16:45:30 -0500

    task 7

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangles"""
+
+
+class Rectangle:
+    """Initialization and methods"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return perimeter of rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rect_matrix = []
+        for _ in range(self.__height):
+            rect_matrix = [str(self.print_symbol) * self.__width]
+        return "\n".join(rect_matrix)
+
+    def __repr__(self):
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print(f"Bye rectangle...")
+
