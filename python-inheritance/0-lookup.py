951ad57592c42cb59bda9543b3c119d8f7eb1515
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-22 20:28:38 -0500

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""function that returns the list of available 
+attributes and methods of an object"""
+
+
 def lookup(obj):
+    """method to return list of all content"""
     return dir(obj)
