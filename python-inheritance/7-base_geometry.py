f749b9b60b65ce900858769c70a6d2129bdc012e
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-23 14:59:59 -0500

    task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,6 +5,7 @@
 class BaseGeometry:
     """one mehtod"""
     def area(self):
+        """public instance method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
