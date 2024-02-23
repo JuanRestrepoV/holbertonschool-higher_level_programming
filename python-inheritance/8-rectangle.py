67acf1ecc3193aa8a67e3c673b43504f0543986b
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-23 15:17:46 -0500

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,18 +2,7 @@
 """Class BaseGeometry"""
 
 
-class BaseGeometry:
-    """one mehtod"""
-    def area(self):
-        """public instance method"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
