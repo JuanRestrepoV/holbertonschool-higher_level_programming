967df67eb743fe37a2c2a61b26e0e733a5d29a90
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-23 08:44:23 -0500

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class MyList that inherits from list"""
+
+class MyList(list):
+    """class Mylist"""
+    def print_sorted(self):
+        new_list = sorted(self)
+        print(new_list)
