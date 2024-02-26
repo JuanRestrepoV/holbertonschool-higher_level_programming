0e805fefcb17b8b9ff59660fcc21d0fe8c5ba1f7
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-25 23:06:12 -0500

    task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -8,7 +8,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
- 
+
     def to_json(self, attrs=None):
         dictionary = {}
         if hasattr(self, '__dict__'):
@@ -17,7 +17,7 @@ class Student:
                     dictionary[key] = value
             return dictionary
         return self.__dict__
-    
+
     def reload_from_json(self, json):
         for key, value in json.items():
             setattr(self, key, value)
