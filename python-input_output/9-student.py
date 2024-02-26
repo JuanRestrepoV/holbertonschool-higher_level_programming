4a22a6a68c8d4695f7f7f5212a9c8fc34305391c
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-25 20:51:53 -0500

    task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """defines the class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        dictionary = {}
+        if hasattr(self, '__dict__'):
+            for key, value in self.__dict__.items():
+                if not callable(value):
+                    dictionary[key] = value
+        return dictionary
