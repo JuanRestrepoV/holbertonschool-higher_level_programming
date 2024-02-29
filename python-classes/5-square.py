bf1ca71e49bc392fb432d65c99a694228434bf99
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-29 17:07:47 -0500

    task 1

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,11 +6,7 @@ class Square:
     """Square with size"""
     def __init__(self, size=0):
         """constructor of square object."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an intenger")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
     @property
     def size(self):
         """Getter for size"""
@@ -26,10 +22,10 @@ class Square:
 
     def area(self):
         """returns the area of the square"""
-        return self.__size ** 2
+        return self.size ** 2
     def my_print(self):
         """prints in stdout the square with #"""
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
             for i in range(self.__size):
