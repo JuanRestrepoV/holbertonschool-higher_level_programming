8f8aa82402bd2bba212610604307894584b0d94f
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-02-29 16:52:28 -0500

    task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,9 +6,10 @@ of this project"""
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-            if id is None:
-                Base.__nb_objects += 1
-                self.id = Base.__nb_objects
-            else:
-                self.id = id
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
