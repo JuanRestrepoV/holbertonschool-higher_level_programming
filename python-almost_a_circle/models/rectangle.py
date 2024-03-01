a6c2955511c1bbb5c9d5b712115600f2543c236a
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-03-01 14:57:51 -0500

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,10 @@ class Rectangle(Base):
             _type_: _description_
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
