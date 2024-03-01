0e0bdc142467c8bc4731eb645aa1ec43b42ffd34
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-03-01 15:35:40 -0500

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -86,5 +86,9 @@ class Rectangle(Base):
         """
         for i in range(self.height):
             print("#" * self.width)
+
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return (
+            "[Rectangle] ({}) {}/{} - {}/{}".format
+            (self.id, self.x, self.y, self.width, self.height)
+        )
