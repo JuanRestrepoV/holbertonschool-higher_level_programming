a54421689ce5d6c677472a42a607baa5f15775b5
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-04-05 19:28:58 -0500

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
